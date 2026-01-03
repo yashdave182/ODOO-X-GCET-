a57bb3260a4871c61feccf7997679d4d66a75050
yashdave182 <yashdave182@gmail.com>
2026-01-03 14:10:09 +0530

    merge conflict resolved

---
+++ backend/app/models/attendance.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, Date, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, date
+
+from app.database import Base
+
+
+class Attendance(Base):
+    __tablename__ = "attendance"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    attendance_date = Column(Date, default=date.today, nullable=False)
+    check_in_time = Column(DateTime, nullable=False)
+    check_out_time = Column(DateTime, nullable=True)
+
+    user = relationship("User")
